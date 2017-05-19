b557fdb7ef8f55406214b599b9fe7261a23000aa
Will Laco <wlaco27@gmail.com>
2017-05-19 09:30:58 -0400

    Sign-in screen done

--- Udemy Snapchat/SignInViewController.swift
+++ Udemy Snapchat/SignInViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 class SignInViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
@@ -18,11 +18,30 @@ class SignInViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func swagifyTapped(_ sender: Any) {
         
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+            if error != nil {
+                
+                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!, completion: { (user, error)
+                    in
+                    print("We tried to create a user")
+                    
+                    if error != nil {
+                        print("**ERROR**\(error!)")
+                    } else {
+                        print("Created user successfully")
+                        self.performSegue(withIdentifier: "signInSegue", sender: nil)
+
+                    }
+                })
+            } else {
+                print("Sign in successful")
+                self.performSegue(withIdentifier: "signInSegue", sender: nil)
+            }
+        }
         
-    
     }
     
 }
---
+++ Udemy Snapchat/SnapsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SnapsViewController.swift
+//  Udemy Snapchat
+//
+//  Created by Will Laco on 5/19/17.
+//  Copyright © 2017 Will Laco. All rights reserved.
+//
+
+import UIKit
+
+class SnapsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func logOutTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+
+}
