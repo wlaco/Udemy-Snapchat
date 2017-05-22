71d0c8c1bd9a47729cebc1c630f9bde741f62442
Will Laco <wlaco27@gmail.com>
2017-05-21 21:34:16 -0400

    addPicVC created. Adding camera functionality next

---
+++ Udemy Snapchat/AddPicViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPicViewController.swift
+//  Udemy Snapchat
+//
+//  Created by Will Laco on 5/21/17.
+//  Copyright © 2017 Will Laco. All rights reserved.
+//
+
+import UIKit
+
+class AddPicViewController: UIViewController {
+
+    @IBOutlet weak var snapImageView: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var snapNameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+        
+    }
+    
+}
